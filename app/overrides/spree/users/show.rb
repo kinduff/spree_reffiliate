75e551d54a160f2a496de2ca264ae6de1c762c24
kinduff <abarcadabra@gmail.com>
2014-09-07 02:44:25 -0300

    Removes user list from user account

--- app/overrides/spree/users/show.rb
+++ app/overrides/spree/users/show.rb
@@ -10,32 +10,3 @@ Deface::Override.new(
 <dd><%= @user.referral_count%></dd>
 CODE
 end
-Deface::Override.new(
-  :virtual_path => "spree/users/show",
-  :name => "user_account",
-  :insert_after => "[data-hook='account_my_orders']"
-) do
-<<-CODE.chomp
-<h3>Referral Information</h3>
-<% if @user.referral_count > 0 %>
-  <table>
-    <thead>
-    <tr>
-      <th></th>
-      <th>Email</th>
-    </tr>
-    </thead>
-    <tbody>
-      <% @user.referral.referred_users.sort_by(&:created_at).to_enum.with_index.reverse_each do |user, i| %>
-        <tr class="<%= cycle('even', 'odd') %>">
-          <td><%= i+1 %></td>
-          <td><%= user.email %></td>
-        </tr>
-      <% end %>
-    </tbody>
-  </table>
-<% else %>
-  <p>No referred users yet.</p>
-<% end %>
-CODE
-end
